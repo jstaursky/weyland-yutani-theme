34dd2d2f840e1299e81a7aa78ed8e7b2fbbe02cd
Joe <jnstaursky@gmail.com>
2020-02-29 20:20:27 -0500

    make foreground not bold

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -65,7 +65,7 @@
 	`(font-lock-constant-face ((,class (:foreground ,const))))
         `(font-lock-doc-face ((,class (:foreground ,comment))))
         `(font-lock-function-name-face ((,class (:foreground ,func :bold t))))
-        `(font-lock-keyword-face ((,class (:bold ,class :foreground ,keyword))))
+        `(font-lock-keyword-face ((,class (:foreground ,keyword))))
         `(font-lock-string-face ((,class (:foreground ,str))))
         `(font-lock-type-face ((,class (:foreground ,type ))))
         `(font-lock-variable-name-face ((,class (:foreground ,var))))

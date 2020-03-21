40b9957349ba9cefe351d9e7719b7a5387d6d050
joe <jnstaursky@gmail.com>
2020-03-21 19:50:00 -0400

    made function names bold + italic

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -140,7 +140,8 @@
     `(font-lock-reference-face ((,class (:foreground ,const))))
     `(font-lock-constant-face ((,class (:foreground ,const ))))
         `(font-lock-doc-face ((,class (:foreground ,comment))))
-        `(font-lock-function-name-face ((,class (:foreground ,func  :background "#2B272E"))))
+        `(font-lock-function-name-face ((,class (:foreground ,func  :background
+                                                             "#2B272E" :italic t :bold t))))                                                   ;:bold t
         `(font-lock-keyword-face ((,class (:foreground ,keyword )))) ;:bold t
         `(font-lock-type-face ((,class (:foreground ,type ))))
         `(font-lock-variable-name-face ((,class (:foreground ,var ))))

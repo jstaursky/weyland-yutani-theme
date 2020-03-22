671fd57a3d1d5b896a50cf61d0e66d99a1469b73
joe <jnstaursky@gmail.com>
2020-03-21 20:54:50 -0400

    remove bold + italic + background to funcs

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -140,8 +140,11 @@
     `(font-lock-reference-face ((,class (:foreground ,const))))
     `(font-lock-constant-face ((,class (:foreground ,const ))))
         `(font-lock-doc-face ((,class (:foreground ,comment))))
-        `(font-lock-function-name-face ((,class (:foreground ,func  :background
-                                                             "#2B272E" :italic t :bold t))))                                                   ;:bold t
+        `(font-lock-function-name-face ((,class (:foreground ,func
+                                                             ;; :background
+                                                             ;; "#2B272E"
+                                                             ;; :italic t :bold t
+                                                             ))))                                                   ;:bold t
         `(font-lock-keyword-face ((,class (:foreground ,keyword )))) ;:bold t
         `(font-lock-type-face ((,class (:foreground ,type ))))
         `(font-lock-variable-name-face ((,class (:foreground ,var ))))

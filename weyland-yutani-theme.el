e9e910c68c29dcf49684fe36592b6cb739aa890b
joe <jnstaursky@gmail.com>
2020-04-19 17:51:37 -0400

    testing brighter bg

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -46,7 +46,7 @@
      (fg2        "#b3c1c9")
      (fg3        "#9babb7")
      (fg4        "#8694a5")
-     (bg1        "#222630")             ;#222630
+     (bg1        "#282c34")             ;#222630 #282C34 #282c34
      (bg2        (color-lighten-name bg1 3))
      (bg3        (color-lighten-name bg2 3))
      (bg4        "#3c3a50")

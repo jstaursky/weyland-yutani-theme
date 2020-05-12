c804d7da1bc22726587203d097728a90eb2923f3
joe <jnstaursky@gmail.com>
2020-05-11 22:23:58 -0400

    update

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -57,7 +57,7 @@
      (keyword    "#90b55b")                    ;#95bb62
      (type       "#9684f2")
      (func       "#d38aea")             ;#d58aad #cc8ae3 #e692e1 #d088ea #de63bf
-     (var        "#a967dd")             ;#bc6fd6 #c678dd #bb73d0 #b569e2 #ba81e1 #b569e2
+     (var        "#b370e7")             ;#bc6fd6 #c678dd #bb73d0 #b569e2 #ba81e1 #b569e2
      (builtin    "#9684f2")
      (const      "#54aae5")
      (negate     "#c5b159")

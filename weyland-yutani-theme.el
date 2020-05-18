457724c8032e6cc052d604aa9be2a3e68be06081
joe <jstaursky@gmail.com>
2020-05-18 13:58:28 -0400

    update gold.

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -57,7 +57,7 @@
      (keyword    "#91b562")             ;#90b55f
      (type       "#968ae6")             ;#a898ff
      (func       "#c588d8")             ;#d18ff6 #d490e9
-     (var        "#bfa681")             ;#bfa681
+     (var        "#a7a074")             ;#bfa681
      (builtin    "#968ae6")             ;#C278FF
      (const      "#5ea9e0")
      (negate     "#c5b159")

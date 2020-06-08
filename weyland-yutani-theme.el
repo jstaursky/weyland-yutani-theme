032fd03a37b9f257f9f475c2c906add8c4fc0039
joe <jstaursky@gmail.com>
2020-06-08 17:13:02 -0400

    lighter gold.

--- weyland-yutani-theme.el
+++ weyland-yutani-theme.el
@@ -55,7 +55,8 @@
                                         ;                          *redesign*
      (type       "#8a7ced")             ;#9683f2  #a48fff  #9787ED  #8a7cda  #8d7fdf
      (func       "#b27cd4")             ;#ca95ed  #c08ee1  #CD9CF0 *redesign*
-     (var        "#999262")             ;#a59d71  #a8a073  #9E9666  #a69e6b *rebalance*
+     (var        "#979d66")             ;#a59d71  #a8a073  #9E9666  #a69e6b *rebalance*
+     ;; #999262
      (builtin    "#8a7ced")
      (const      "#5591bd")             ;#54b2f2  #50aae7  #6BB7EE  #62a8db  #5693c0
      (negate     "#c5b159")
